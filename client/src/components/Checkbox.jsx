b638a1d25b3eb8771c7c57f8624fdb4eedd9183b
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-10-02 23:14:35 +0200

    :sparkles: new compare toggle to chose phones to compare

---
+++ client/src/components/Checkbox.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+
+export default function Checkbox({ label, value, onChange, disabled }) {
+  return (
+    <label htmlFor={label} className="compare-checkbox">
+      <input
+        type="checkbox"
+        value={value}
+        onChange={onChange}
+        name={label}
+        disabled={disabled}
+      />
+      {label}
+    </label>
+  );
+}
+Checkbox.propTypes = {
+  label: PropTypes.string.isRequired,
+  value: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+};
+
+Checkbox.defaultProps = {
+  disabled: false,
+};
--- client/src/components/PhoneCard.jsx
+++ client/src/components/PhoneCard.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
+import Checkbox from "./Checkbox";
 
-export default function PhoneCard({ phone }) {
+export default function PhoneCard({ phone, compare, handleCompare }) {
+  const [checked, setChecked] = useState(compare.includes(phone.phone_id));
+
+  const handleCheck = () => {
+    handleCompare(phone.phone_id);
+    setChecked(!checked);
+  };
   return (
     <section key={phone.phone_id} className="phone-card">
+      <Checkbox
+        label="Compare"
+        value={checked}
+        onChange={handleCheck}
+        disabled={!checked && compare.length >= 3}
+      />
       <img src={phone.picture} alt={phone.model} />
       <div className="phone-card-text">
         <p className="phone-card-brand">{phone.brand_name}</p>
@@ -19,4 +33,6 @@ PhoneCard.propTypes = {
     brand_name: PropTypes.string.isRequired,
     model: PropTypes.string.isRequired,
   }).isRequired,
+  compare: PropTypes.arrayOf(PropTypes.number).isRequired,
+  handleCompare: PropTypes.func.isRequired,
 };
