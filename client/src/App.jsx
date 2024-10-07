284365ff73e69b8b2d21bee303c38dfe9dfe9702
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-10-07 15:04:59 +0200

    start of phone details section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 
 import "./styles/App.css";
 import "./styles/Phones.css";
+import "./styles/Compare.css";
 
 function App() {
   return (
--- client/src/pages/Compare.jsx
+++ client/src/pages/Compare.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import CompareContext from "../contexts/CompareContext";
 import { getPhonesById } from "../services/phoneRequests";
+import CompareCard from "../components/CompareCard";
 
 export default function Compare() {
   const [phones, setPhones] = useState([]);
@@ -11,7 +12,6 @@ export default function Compare() {
       try {
         const phonesData = await getPhonesById(compared);
         setPhones(phonesData);
-        console.info(phonesData);
       } catch (error) {
         console.error(error);
         // toast error
@@ -23,10 +23,23 @@ export default function Compare() {
     }
   }, [compared]);
 
-  console.info(phones);
   return (
-    <section>
+    <div>
       <h1>Phones comparison</h1>
-    </section>
+      <section className="phone-details-container">
+        {phones.map((phone) => (
+          <div key={phone.phone_id} className="phone-details">
+            <CompareCard phone={phone} />
+            <div className="phone-details-release">
+              <p>Release year : {phone.release_year}</p>
+              <p>Price at release : {phone.price}€</p>
+            </div>
+            <div className="phone-details-screen">
+              <p>{phone.screen_type_name}</p>
+            </div>
+          </div>
+        ))}
+      </section>
+    </div>
   );
 }
