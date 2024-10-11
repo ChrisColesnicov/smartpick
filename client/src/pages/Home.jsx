0c69e6fa96f92f85fadc9d3da740fe52c2070c65
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-10-11 19:11:48 +0200

    home page is reworked to have feature section, compare page changed accordingly, wip

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import Slider from "react-slick";
+import { useLoaderData } from "react-router-dom";
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import random from "../services/utils";
 
 import banner1 from "../assets/images/banner-1.webp";
 import banner2 from "../assets/images/banner-2.webp";
 import banner3 from "../assets/images/banner-3.webp";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import CompareCard from "../components/CompareCard";
 
 export default function Home() {
   const settings = {
@@ -17,6 +21,22 @@ export default function Home() {
     slidesToScroll: 1,
     arrows: false,
   };
+  const featuredSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+  };
+  const phones = useLoaderData();
+  const featuredPhones = [
+    phones[random(0, phones.length - 1)],
+    phones[random(0, phones.length - 1)],
+    phones[random(0, phones.length - 1)],
+  ];
+  console.info(featuredPhones);
+
   return (
     <div>
       <header role="banner">
@@ -26,7 +46,18 @@ export default function Home() {
           <img src={banner3} alt="banner 3" className="carousel-img" />
         </Slider>
       </header>
-      <section>Featured</section>
+      <section>
+        <h2 className="secondary-title">Featured phones</h2>
+        <Slider {...featuredSettings} className="featured-carousel">
+          {featuredPhones.map((phone) => (
+            <CompareCard
+              key={phone.phone_id}
+              phone={phone}
+              className="featured"
+            />
+          ))}
+        </Slider>
+      </section>
       <section>Browse and compare</section>
     </div>
   );
