fa53bfa4ff8f4e82876d3f2d7396b210c72c3418
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-10-21 17:04:12 +0200

    add all routes for user

--- server/app/routers/api/users/router.js
+++ server/app/routers/api/users/router.js
@@ -2,8 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
+const userActions = require("../../../controllers/userActions");
+
 /* ************************************************************************* */
 // Import And Use Routers Here
 /* ************************************************************************* */
+
+router.get("/", userActions.browse);
+router.get("/", userActions.read);
+router.post("/", userActions.add);
+router.put("/", userActions.edit);
+router.delete("/", userActions.destroy);
 
 module.exports = router;
