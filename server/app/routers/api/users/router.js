365785cf84e667e4b07d95c9ce946525c2144d82
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-10-21 16:56:42 +0200

    create user router

--- server/app/routers/api/users/router.js
+++ server/app/routers/api/users/router.js
@@ -6,12 +6,4 @@ const router = express.Router();
 // Import And Use Routers Here
 /* ************************************************************************* */
 
-const phonesRouter = require("./phones/router");
-const usersRouter = require("./users/router");
-
-router.use("/phones", phonesRouter);
-router.use("/users", usersRouter);
-
-/* ************************************************************************* */
-
 module.exports = router;
